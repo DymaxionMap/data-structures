47fdba1f0c77ca75de07f6dda438f3016356b073
DymaxionMap <sthliao@gmail.com>
2018-11-08 15:56:20 -0800

    Complete all tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,33 +5,34 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     // if the head and tail of list are null
+    var newNode = Node(value);
     if (list.head === null && list.tail === null) {
       // create a new node with value of value
-      var node = Node(value);
       // set list head and list tail to node 
-      list.head = node;
-      list.tail = node;
+      list.head = newNode;
+      list.tail = newNode;
     } else {
-      var newNode = Node(value);
       list.tail.next = newNode;
       list.tail = newNode;
     }
   };
 
   list.removeHead = function() {
+    var value = list.head.value;
+    list.head = list.head.next;
+    return value;
   };
 
   list.contains = function(target) {
-  // FOR REFERENCE
-    // if the head and tail of list are set to different nodes (i.e. more than one node) 
-    //   set currentNode to head
-    //   while currentNode.next is not null 
-    //     set currentNode = currentNode.next
-    //   (currentNode is set to tail now)
-    //   create a new node with value of value
-    //   currentNode.next = new node
-    //   currentNode = currentNode.next
-    //   list.tail = currentNode
+    var current = list.head;
+    while (current !== null) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
   };
 
   return list;
@@ -49,5 +50,10 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// removeHead and addTail are O(1) because you 
+// already have a reference where the first and last nodes are.
+// contains is O(n) because you will have to traverse through the whole list in the worst case.
+
 
 
