61ff8f5dae5b5ceebcfddadac90b53a43ea30200
DymaxionMap <sthliao@gmail.com>
2018-11-09 20:13:05 -0800

    Complete tests for Binary Search Tree up to depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,55 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(treePrototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+};
+
+var treePrototype = {};
+
+treePrototype.insert = function (value) {
+  var traverseTree = function (tree) {
+    if (value > tree.value) {
+      if (tree.right === null) {
+        tree.right = BinarySearchTree(value);
+      } else {
+        traverseTree(tree.right);
+      }
+    } else {
+      if (tree.left === null) {
+        tree.left = BinarySearchTree(value);
+      } else {
+        traverseTree(tree.left);
+      }
+    }
+  };
+
+  traverseTree(this);
+};
+
+treePrototype.contains = function (value) {
+  var found = false;
+  var traverseTree = function (tree) {
+    if (tree === null) {
+      return;
+    }
+
+    if (value === tree.value) {
+      found = true;
+    } else if (value < tree.value) {
+      traverseTree(tree.left);
+    } else {
+      traverseTree(tree.right);
+    }
+  };
+  
+  traverseTree(this);
+  return found;
+};
+
+treePrototype.depthFirstLog = function (cb) {
+  
 };
 
 
