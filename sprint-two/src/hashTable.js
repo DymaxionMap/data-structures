831718cedffd667f9d1fe7733e5c2dac60179f6b
DymaxionMap <sthliao@gmail.com>
2018-11-10 17:04:03 -0800

    Partially complete implementation of hash table advanced content

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -22,6 +23,39 @@ HashTable.prototype.insert = function(k, v) {
   bucket.push(tuple);
   
 };
+
+///////////////////////////////////////////////////////////////
+
+// PARTIAL IMPLEMENTATION OF ADVANCED CONTENT 
+
+// HashTable.prototype.insert = function(k, v) {
+//   // Increment count
+//   // If size threshold is exceeded
+//     // Rehash
+//   // Call _insert
+// };
+
+// HashTable.prototype._rehash = function(sizeFactor) {
+//   // set this._limit to sizeFactor * this._limit
+//   var newLimit = this._limit * sizeFactor;
+//   // create a storage object using this._limit
+//   var newStorage = LimitedArray(this._limit);
+//   // for each bucket in old this._storage
+//   for(var i = 0; i < this._limit; i++) {
+//     var bucket = newStorage.get(i);
+//     for(var j = 0; j < bucket[i].length; j++) {
+//       newStorage._insert(bucket[i]);      
+//     }
+
+//     // for each tuple in bucket
+//       // newStorage._insert(tuple)
+//   }
+//   this._limit = newLimit;
+//   this._storage = newStorage;
+//   //this._storage = newStorage
+// };
+
+/////////////////////////////////////////////////////////////////////
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
